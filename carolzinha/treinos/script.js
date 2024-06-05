b40d19ffd733bbe28d9c6ee8880c95326bcae5ff
mateusnbrito <mateusnascbrito@gmail.com>
2024-06-05 19:11:24 -0300

    add json content to code

--- carolzinha/treinos/script.js
+++ carolzinha/treinos/script.js
@@ -1,3 +1,4 @@
+let $listOfTrainings = document.querySelector("#list-of-trainings");
 let usersListOfTraining;
 
 fetch("../../resources/jsons/treino-de-carol.json")
@@ -9,5 +10,7 @@ fetch("../../resources/jsons/treino-de-carol.json")
   });
 
 function populateTrainings() {
-  console.log(usersListOfTraining);
+  usersListOfTraining.forEach(training => {
+    $listOfTrainings.insertAdjacentHTML("beforeend", `<li class="list-item"><a href="/carolzinha/treinos/treino/?id=${training.id}">${training.title}</a></li>`);
+  });
 }
